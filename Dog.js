9cbfb31da302293dbe64700497c7409e0794bd4b
hanh <dangvhanh96@gmail.com>
2021-07-23 17:05:13 +0700

    Add index file

--- Dog.js
+++ Dog.js
@@ -1,7 +1,13 @@
-function Dog() {
+function Dog(name) {
     this.stomach = [];
+    this.name = name;
 
 };
 Dog.prototype.eat = function() {
     this.stomach.push(cat)
 };
+Dog.prototype.hi = function() {
+    console.log('I am' + this.name )
+};
+
+module.exports = Dog;
